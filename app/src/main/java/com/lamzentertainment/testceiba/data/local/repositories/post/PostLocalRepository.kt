3678aca91a94c91fffd9f26bfc6d8b87dc6535e7
Marlon Mendez <marlon@Marlons-MacBook-Air.local>
2022-04-25 00:35:30 -0500

    loocals repostitories

--- app/src/main/java/com/lamzentertainment/testceiba/data/local/repositories/post/PostLocalRepository.kt
+++ app/src/main/java/com/lamzentertainment/testceiba/data/local/repositories/post/PostLocalRepository.kt
@@ -1,22 +1,63 @@
 package com.lamzentertainment.testceiba.data.local.repositories.post
 
+import android.content.Context
+import com.google.gson.Gson
+import com.lamzentertainment.testceiba.data.local.config.LocalSqlite
+import com.lamzentertainment.testceiba.data.local.config.tables.PostSqlite
+import com.lamzentertainment.testceiba.data.local.dto.PostLocalDto
+import com.lamzentertainment.testceiba.data.local.dto.UserLocalDto
 import com.lamzentertainment.testceiba.domain.entities.PostEntity
+import com.lamzentertainment.testceiba.domain.entities.UserEntity
 import com.lamzentertainment.testceiba.domain.repositories.IPostRepository
+import org.json.JSONArray
+import org.json.JSONObject
 
-class PostLocalRepository : IPostRepository {
+class PostLocalRepository(private val context: Context) : IPostRepository {
     override suspend fun getPosts(): List<PostEntity> {
-        TODO("Not yet implemented")
+        return getFromSqlite("SELECT * FROM ${PostSqlite.TABLE_NAME}")
     }
 
     override suspend fun getPostsOfUserByUserId(userId: Int): List<PostEntity> {
-        TODO("Not yet implemented")
+        return getFromSqlite("SELECT * FROM ${PostSqlite.TABLE_NAME} Where ${PostSqlite.COLUMN_USER_ID} = $userId")
     }
 
     override suspend fun savePosts(posts: List<PostEntity>): Boolean {
-        TODO("Not yet implemented")
+        var responseFinal = true
+        posts.forEach{
+            val response = savePost(it)
+            if(!response){
+                responseFinal = false
+            }
+        }
+        return responseFinal
     }
 
     override suspend fun savePost(post: PostEntity): Boolean {
-        TODO("Not yet implemented")
+        val dbHelper = LocalSqlite(context)
+        return try {
+            dbHelper.writableDatabase.execSQL("" +
+                    "INSERT INTO ${PostSqlite.TABLE_NAME}" +
+                    " (${PostSqlite.COLUMN_ID}, ${PostSqlite.COLUMN_BODY}, ${PostSqlite.COLUMN_TITLE}," +
+                    " ${PostSqlite.COLUMN_USER_ID})" +
+                    " VALUES (?, ?, ?, ?)",
+                arrayOf(post.id, post.body, post.title, post.userId))
+            true
+        }catch (e: Exception){
+            false
+        }
+    }
+
+    private fun getFromSqlite(sql: String): List<PostEntity> {
+        val dbHelper = LocalSqlite(context)
+        return try {
+            val posts : MutableList<PostEntity> = mutableListOf()
+            dbHelper.readableDatabase.rawQuery(sql, null).use {
+                val postGetted = PostLocalDto.toPost(it)
+                posts.add(postGetted)
+            }
+            posts.toList()
+        }catch (e: Exception){
+            listOf()
+        }
     }
 }
--- app/src/main/java/com/lamzentertainment/testceiba/data/local/repositories/user/UserLocalRepository.kt
+++ app/src/main/java/com/lamzentertainment/testceiba/data/local/repositories/user/UserLocalRepository.kt
@@ -1,22 +1,66 @@
 package com.lamzentertainment.testceiba.data.local.repositories.user
 
+import android.content.Context
+import com.lamzentertainment.testceiba.data.local.config.LocalSqlite
+import com.lamzentertainment.testceiba.data.local.config.tables.PostSqlite
+import com.lamzentertainment.testceiba.data.local.config.tables.UserSqlite
+import com.lamzentertainment.testceiba.data.local.dto.PostLocalDto
+import com.lamzentertainment.testceiba.data.local.dto.UserLocalDto
+import com.lamzentertainment.testceiba.domain.entities.PostEntity
 import com.lamzentertainment.testceiba.domain.entities.UserEntity
 import com.lamzentertainment.testceiba.domain.repositories.IUserRepository
 
-class UserLocalRepository : IUserRepository {
+class UserLocalRepository(private val context: Context) : IUserRepository {
     override suspend fun getUsers(page: Int, word: String): List<UserEntity> {
-        TODO("Not yet implemented")
+        return getFromSqlite("SELECT * FROM ${UserSqlite.TABLE_NAME}")
     }
 
     override suspend fun getUser(id: Int): UserEntity? {
-        TODO("Not yet implemented")
+        return try {
+            getFromSqlite("SELECT * FROM ${UserSqlite.TABLE_NAME} WHERE ${UserSqlite.COLUMN_ID} = $id").first()
+        }catch (e: Exception){
+            null
+        }
     }
-
     override suspend fun saveUser(user: UserEntity): Boolean {
-        TODO("Not yet implemented")
-    }
+        val dbHelper = LocalSqlite(context)
+        return try {
+            dbHelper.writableDatabase.execSQL("" +
+                    "INSERT INTO ${UserSqlite.TABLE_NAME}" +
+                    " (${UserSqlite.COLUMN_NAME}, ${UserSqlite.COLUMN_EMAIL}," +
+                    " ${UserSqlite.COLUMN_PHONE})" +
+                    " VALUES (?, ?, ?, ?)",
+                arrayOf(user.id,
+                    user.name,
+                    user.email,
+                    user.phone))
+            true
+        }catch (e: Exception){
+            false
+        }    }
 
     override suspend fun saveUsers(users: List<UserEntity>): Boolean {
-        TODO("Not yet implemented")
+        var responseFinal = true
+        users.forEach{
+            val response = saveUser(it)
+            if(!response){
+                responseFinal = false
+            }
+        }
+        return responseFinal
+    }
+
+    private fun getFromSqlite(sql: String): List<UserEntity> {
+        val dbHelper = LocalSqlite(context)
+        return try {
+            val users : MutableList<UserEntity> = mutableListOf()
+            dbHelper.readableDatabase.rawQuery(sql, null).use {
+                val userGetted = UserLocalDto.toUser(it)
+                users.add(userGetted)
+            }
+            users.toList()
+        }catch (e: Exception){
+            listOf()
+        }
     }
 }
